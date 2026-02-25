6f2575df29c4e4b53ecbfd1ba7ec0a02aac5b764
aishwarya sriharsha <aishwaryamulkundkar98@gmail.com>
2026-02-25 19:05:41 +0530

    Conversation between Aishwarya and Harsha

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Aishwarya:: hi harsha , How are you?"
+echo "Harsha  :: Hello Aishwarya, I am Fine. How are you ?"
+echo  "Aishwarya:: what are you learning now a days ?"
+echo "Harsha :: I am learning shell scripting aishwarya"
